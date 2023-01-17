06c04abf04d171a3f8084f24ded9743fb47bf99f
Mireia Olivé <mirisfu@gmail.com>
2023-01-17 22:23:13 +0100

    update

--- N1E2_pizzeria/pizzeria.sql
+++ N1E2_pizzeria/pizzeria.sql
@@ -94,4 +94,15 @@ INSERT INTO `pizzeria`.`botiga` (`botiga_id`, `carrer`, `numero`, `codi_postal`,
 INSERT INTO `pizzeria`.`botiga` (`botiga_id`, `carrer`, `numero`, `codi_postal`, `localitat`, `provincia`) VALUES (4, 'Lesseps, Plaça de', '1', '09885', 'Cambrils', 'Tarragona');
 INSERT INTO `pizzeria`.`botiga` (`botiga_id`, `carrer`, `numero`, `codi_postal`, `localitat`, `provincia`) VALUES (5, 'Rajoler, Carrer del', '5', '09877', 'Mollerussa', 'Lleida');
 
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (1, 'Roman ', 'Navarrete', 'Rambla, La', '08996', 'Barcelona', '655443387');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (2, 'Noa', 'Villena', 'Laietana, Via ', '54336', 'Girona', '566887790');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (3, 'Uxia ', 'Martínez', 'Lírica, Carrer de la', '07665', 'Girona', '633478559');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (4, ' Michael ', 'Pinilla ', 'Jaén, Carrer de', '09887', 'Barcelona', '655447890');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (5, ' Soraya ', 'Mir ', ' Xipre, Carrer de', '02334', 'Lleida', '622345699');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (6, 'Asuncion ', 'Alcantara', 'León, Passatge de', '01876', 'Barcelona', '640998776');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (7, 'Maria ', 'Pallares ', 'apineria, Carrer de la', '08714', 'Barcelona', '633221543');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (8, 'Markel ', 'Rodrigues ', 'Olesa, Carrer d\'', '01234', 'Barcelona', '644557889');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (9, 'Marcela ', 'Torregrosa', 'Nau, Carrer de la', '07763', 'Tarragona', '666645337');
+INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (10, 'Mari ', 'Toledano ', 'Nínive, Carrer de', '01234', 'tarragona', '699885463');
+
 
