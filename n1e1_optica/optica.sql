236bd12886a784e54479326560194cea9c9f21c0
Mireia Olivé <mirisfu@gmail.com>
2023-01-17 11:32:17 +0100

    update

--- n1e1_optica/optica.sql
+++ n1e1_optica/optica.sql
@@ -84,7 +84,21 @@ CREATE TABLE IF NOT EXISTS `optica`.`ulleres` (
     REFERENCES `optica`.`empleat` (`empleat_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
+ENGINE = InnoDB;
+
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (1, 'Marta Romero', 'Agla', '15', '1', '3', '08002', 'Espanya', '655743320', 'm.romero@gmail.com', '2023-01-01 17:17:02');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (2, 'Sergi Garcia', 'Alexandre ', '2', '3', '4', '08024 ', 'Espanya', '655734435', 's.garcia@gmail.com', '2023-01-02 18:56:30');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (3, 'Francisco Lopez', 'Ametllers', '3', '2', '4', '08006', 'Espanya', '678993324', 'f.lopez@hotmail.com', '2023-01-03 14:56:30');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (4, 'Mariana Sanchez', 'Odissea', '5', '4', '1', '08004', 'Espanya', '644786645', 'm.sanchez@gmail.com', '2023-01-05 13:20:30');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (5, 'Tomas Batlleve', 'Rectoret', '3', '2', '1', '08007', 'Espanya', '622885460', 't.batlleve@hotmail.com', '2023-01-04 12:56:30');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (6, 'Joan Gili', 'Lepant', '23', '2', '1', '08980', 'Espanya', '650884322', 'j.gili@gmail.com', '2023-01-03 18:17:02');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (7, 'Moritz Fernandez', 'Jaume Cabrera', '55', '3', '1', '08004', 'Espanya', '677870322', 'm.fernandez@hotmail.com', '2023-01-03 18:50:02');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (8, 'Sara Besnard', 'Tapioles', '120', '4', '3', '08001', 'Espanya', '612344554', 's.besnard@gmail.com', '2023-01-06 10:17:02');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (9, 'Joanthan Perez', 'Madrona Prat', '1', '1', '1', '08004', 'Espanya', '644832255', 'j.perez@hotmail.com', '2023-01-07 12:23:02');
+INSERT INTO `optica`.`client` (`client_id`, `nom`, `carrer`, `numero`, `pis`, `porta`, `codi_postal`, `pais`, `telefono`, `email`, `data_registre`) VALUES (10, 'Montse Garriga', 'Sagunt', '33', '1', '3', '08980', 'Espanya', '936662345', 'm.garriga@gmail.com', '2023-01-08 11:17:02');
+
+
+
 
 
 
