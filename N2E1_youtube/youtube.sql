d4f9c648b48d2d826bf37703a103d406af405750
Mireia Olivé <mirisfu@gmail.com>
2023-01-18 21:18:44 +0100

    update

--- N2E1_youtube/youtube.sql
+++ N2E1_youtube/youtube.sql
@@ -1,5 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS `youtube` DEFAULT CHARACTER SET utf8;
 
+-- create table usuari --
 CREATE TABLE IF NOT EXISTS `youtube`.`usuari` (
   `usuari_id` INT NOT NULL,
   `email` VARCHAR(300) NULL,
@@ -12,6 +13,7 @@ CREATE TABLE IF NOT EXISTS `youtube`.`usuari` (
   PRIMARY KEY (`usuari_id`))
 ENGINE = InnoDB;
 
+-- create table video --
 CREATE TABLE IF NOT EXISTS `youtube`.`video` (
   `video_id` INT NOT NULL,
   `titol` VARCHAR(200) NULL,
@@ -41,6 +43,7 @@ CREATE TABLE IF NOT EXISTS `youtube`.`video` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table subscriure --
 CREATE TABLE IF NOT EXISTS `youtube`.`subscriure` (
   `subscriure_id` INT NOT NULL,
   `canal_id` INT NULL,
@@ -60,6 +63,7 @@ CREATE TABLE IF NOT EXISTS `youtube`.`subscriure` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table canal --
 CREATE TABLE IF NOT EXISTS `youtube`.`canal` (
   `canal_id` INT NOT NULL,
   `nom` VARCHAR(45) NULL,
@@ -75,6 +79,7 @@ CREATE TABLE IF NOT EXISTS `youtube`.`canal` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table comentari --
 CREATE TABLE IF NOT EXISTS `youtube`.`comentari` (
   `comentari_id` INT NOT NULL,
   `text` VARCHAR(500) NULL,
@@ -96,12 +101,14 @@ CREATE TABLE IF NOT EXISTS `youtube`.`comentari` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table etiqueta --
 CREATE TABLE IF NOT EXISTS `youtube`.`etiqueta` (
   `etiqueta_id` INT NOT NULL,
   `nom` VARCHAR(45) NULL,
   PRIMARY KEY (`etiqueta_id`))
 ENGINE = InnoDB;
 
+-- create table LikesDislikes --
 CREATE TABLE IF NOT EXISTS `youtube`.`LikesDislikes` (
   `usuari_id` INT NULL,
   `video_id` INT NULL,
@@ -120,6 +127,7 @@ CREATE TABLE IF NOT EXISTS `youtube`.`LikesDislikes` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table LikesDislikesComentari --
 CREATE TABLE IF NOT EXISTS `youtube`.`LikesDislikesComentari` (
   `usuari_id` INT NULL,
   `comentari_id` INT NULL,
@@ -138,6 +146,7 @@ CREATE TABLE IF NOT EXISTS `youtube`.`LikesDislikesComentari` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table playlist --
 CREATE TABLE IF NOT EXISTS `youtube`.`playlist` (
   `playlist_id` INT NOT NULL,
   `nom` VARCHAR(45) NULL,
@@ -154,4 +163,51 @@ CREATE TABLE IF NOT EXISTS `youtube`.`playlist` (
 ENGINE = InnoDB;
 
 
+-- create data to canal --
+INSERT INTO `youtube`.`canal` (`canal_id`, `nom`, `descripcio`, `publicacio`, `usuari_id`) VALUES (1, 'Hits de los 80', 'Las 100 mejores canciones de los 80 · Eurythmics - Sweet Dreams (Are made of this) (1983) · Yazz - The only way is up (1988) ', '2022-01-12', 1);
+INSERT INTO `youtube`.`canal` (`canal_id`, `nom`, `descripcio`, `publicacio`, `usuari_id`) VALUES (2, 'Yoga', 'Los mejores canales de Youtube para hacer yoga: Xuan Lan yoga. Si consiguió enseñarle a hacer asanas a las triunfitos qué no conseguirá contigo', '2022-01-14', 2);
+INSERT INTO `youtube`.`canal` (`canal_id`, `nom`, `descripcio`, `publicacio`, `usuari_id`) VALUES (3, 'Cocina', 'Las mejores recetas y video recetas de cocina, cocineros y programas de televisión .', '2022-08-12', 3);
+INSERT INTO `youtube`.`canal` (`canal_id`, `nom`, `descripcio`, `publicacio`, `usuari_id`) VALUES (4, 'Arte', 'ARTE en español - Gratis, en español, una selección del canal cultural europeo ARTE · André Malraux · Highlights · Grandes vidas, grandes obras · Líneas torcidas.', '2022-11-12', 4);
+INSERT INTO `youtube`.`canal` (`canal_id`, `nom`, `descripcio`, `publicacio`, `usuari_id`) VALUES (5, 'Cine', 'No te pierdas el cine de estreno directo de la sala de cine a tu casa en Movistar Estrenos y los canales dedicados en exclusiva al cine de todos los género.', '2022-01-30', 5);
+INSERT INTO `youtube`.`canal` (`canal_id`, `nom`, `descripcio`, `publicacio`, `usuari_id`) VALUES (6, 'Teatro', 'Entradas de espectáculos y teatro en Madrid desde 12€: danza, música clásica, ópera, teatro, zarzuela y más de 80 espectáculos diferentes cada año.', '2022-05-12', 6);
+INSERT INTO `youtube`.`canal` (`canal_id`, `nom`, `descripcio`, `publicacio`, `usuari_id`) VALUES (7, 'Aprende ingles', 'Principiantes · Speak English With Mr. Duncan · Easy English · BBC Learning English · Jennifer ESL.', '2022-06-12', 6);
 
+-- create data to comentari --
+INSERT INTO `youtube`.`comentari` (`comentari_id`, `text`, `publicacio`, `usuari_id`, `video_id`) VALUES (2, 'que pena, me das tu telefono?', '2023-01-02 09:04:10', 2, 2);
+INSERT INTO `youtube`.`comentari` (`comentari_id`, `text`, `publicacio`, `usuari_id`, `video_id`) VALUES (3, 'el mejor de los videos', '2022-12-09 09:04:19', 3, 3);
+INSERT INTO `youtube`.`comentari` (`comentari_id`, `text`, `publicacio`, `usuari_id`, `video_id`) VALUES (4, 'no me gusto demasiado', '2023-01-06 09:04:32', 4, 4);
+INSERT INTO `youtube`.`comentari` (`comentari_id`, `text`, `publicacio`, `usuari_id`, `video_id`) VALUES (5, 'menuda master class', '2023-01-11 09:04:42', 5, 5);
+INSERT INTO `youtube`.`comentari` (`comentari_id`, `text`, `publicacio`, `usuari_id`, `video_id`) VALUES (6, 'on repeat toda la semana!', '2023-01-04 09:04:47', 6, 6);
+
+-- create data to etiqueta --
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (1, 'musica');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (2, 'cine');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (3, 'arte');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (4, 'deporte');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (5, 'fotografia');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (6, 'vacaciones');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (7, 'documental');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (8, 'historia');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (9, 'michael jackson');
+INSERT INTO `youtube`.`etiqueta` (`etiqueta_id`, `nom`) VALUES (10, 'queen');
+
+-- create data to LikesDislikes --
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (1, 1, '2023-01-11 09:08:48', true);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (2, 2, '2022-11-10 09:09:51', true);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (3, 3, '2023-01-04 09:09:46', true);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (4, 4, '2022-12-14 09:09:40', false);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (5, 5, '2023-01-03 09:09:34', false);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (6, 6, '2022-12-14 09:09:29', true);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (7, 7, '2022-12-07 09:09:22', true);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (8, 8, '2022-11-17 09:09:15', true);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (9, 9, '2023-01-12 09:09:10', true);
+INSERT INTO `youtube`.`LikesDislikes` (`usuari_id`, `video_id`, `data_hora`, `isLike`) VALUES (10, 10, '2023-01-22 09:09:02', true);
+
+-- create data to LikesDislikesComentari --
+INSERT INTO `youtube`.`LikesDislikesComentari` (`usuari_id`, `comentari_id`, `data_hora`, `isLike`) VALUES (1, 1, '2023-01-04 09:12:42', true);
+INSERT INTO `youtube`.`LikesDislikesComentari` (`usuari_id`, `comentari_id`, `data_hora`, `isLike`) VALUES (1, 2, '2023-01-20 09:12:53', true);
+INSERT INTO `youtube`.`LikesDislikesComentari` (`usuari_id`, `comentari_id`, `data_hora`, `isLike`) VALUES (2, 3, '2023-01-28 09:13:00', true);
+INSERT INTO `youtube`.`LikesDislikesComentari` (`usuari_id`, `comentari_id`, `data_hora`, `isLike`) VALUES (3, 4, '2023-01-29 09:13:07', true);
+INSERT INTO `youtube`.`LikesDislikesComentari` (`usuari_id`, `comentari_id`, `data_hora`, `isLike`) VALUES (4, 5, '2023-01-31 09:13:14', true);
+INSERT INTO `youtube`.`LikesDislikesComentari` (`usuari_id`, `comentari_id`, `data_hora`, `isLike`) VALUES (5, 6, '2023-01-29 09:13:21', true);
+INSERT INTO `youtube`.`LikesDislikesComentari` (`usuari_id`, `comentari_id`, `data_hora`, `isLike`) VALUES (2, 3, '2023-01-28 09:13:00', true);
