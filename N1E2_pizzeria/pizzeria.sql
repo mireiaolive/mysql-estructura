5c4dce9ab181c768fb754369bdd43bcd01def1af
Mireia Olivé <mirisfu@gmail.com>
2023-01-18 17:50:14 +0100

    update

--- N1E2_pizzeria/pizzeria.sql
+++ N1E2_pizzeria/pizzeria.sql
@@ -1,5 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS `pizzeria` DEFAULT CHARACTER SET utf8;
 
+-- create table botiga --
 CREATE TABLE IF NOT EXISTS `pizzeria`.`botiga` (
   `botiga_id` INT NOT NULL,
   `carrer` VARCHAR(45) NULL,
@@ -10,6 +11,7 @@ CREATE TABLE IF NOT EXISTS `pizzeria`.`botiga` (
   PRIMARY KEY (`botiga_id`))
 ENGINE = InnoDB;
 
+-- create table empleat --
 CREATE TABLE IF NOT EXISTS `pizzeria`.`empleat` (
   `empleat_id` INT NOT NULL,
   `nom` VARCHAR(45) NULL,
@@ -25,6 +27,7 @@ CREATE TABLE IF NOT EXISTS `pizzeria`.`empleat` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table comanda --
 CREATE TABLE IF NOT EXISTS `pizzeria`.`comanda` (
   `comanda_id` INT NOT NULL,
   `data_hora` DATETIME NULL,
@@ -55,12 +58,14 @@ CREATE TABLE IF NOT EXISTS `pizzeria`.`comanda` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table pizza --
 CREATE TABLE IF NOT EXISTS `pizzeria`.`pizza` (
   `pizza_id` INT NOT NULL,
   `nom` VARCHAR(45) NULL,
   PRIMARY KEY (`pizza_id`))
 ENGINE = InnoDB;
 
+-- create table producte --
 CREATE TABLE IF NOT EXISTS `pizzeria`.`producte` (
   `producte_id` INT NOT NULL,
   `nom` VARCHAR(45) NULL,
@@ -77,6 +82,7 @@ CREATE TABLE IF NOT EXISTS `pizzeria`.`producte` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- create table repartidor --
 CREATE TABLE IF NOT EXISTS `pizzeria`.`repartidor` (
   `repartidor_id` INT NOT NULL,
   `nom` VARCHAR(45) NULL,
@@ -84,6 +90,7 @@ CREATE TABLE IF NOT EXISTS `pizzeria`.`repartidor` (
   PRIMARY KEY (`repartidor_id`))
 ENGINE = InnoDB;
 
+-- create table client --
 CREATE TABLE IF NOT EXISTS `pizzeria`.`client` (
   `client_id` INT NOT NULL,
   `nom` VARCHAR(45) NULL,
@@ -95,12 +102,14 @@ CREATE TABLE IF NOT EXISTS `pizzeria`.`client` (
   PRIMARY KEY (`client_id`))
 ENGINE = InnoDB;
 
+-- create data to botiga --
 INSERT INTO `pizzeria`.`botiga` (`botiga_id`, `carrer`, `numero`, `codi_postal`, `localitat`, `provincia`) VALUES (1, ' Karl Marx, Plaça de', '1', '08005', 'Barcelona', 'Barcelona');
 INSERT INTO `pizzeria`.`botiga` (`botiga_id`, `carrer`, `numero`, `codi_postal`, `localitat`, `provincia`) VALUES (2, 'Laberint, Carrer del', '34', '08991', 'Martorell', 'Barcelona');
 INSERT INTO `pizzeria`.`botiga` (`botiga_id`, `carrer`, `numero`, `codi_postal`, `localitat`, `provincia`) VALUES (3, 'Lázaro Cárdenas, Carrer de', '33', '08775', 'Platja D\'Aro', 'Girona');
 INSERT INTO `pizzeria`.`botiga` (`botiga_id`, `carrer`, `numero`, `codi_postal`, `localitat`, `provincia`) VALUES (4, 'Lesseps, Plaça de', '1', '09885', 'Cambrils', 'Tarragona');
 INSERT INTO `pizzeria`.`botiga` (`botiga_id`, `carrer`, `numero`, `codi_postal`, `localitat`, `provincia`) VALUES (5, 'Rajoler, Carrer del', '5', '09877', 'Mollerussa', 'Lleida');
 
+-- create data to client --
 INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (1, 'Roman ', 'Navarrete', 'Rambla, La', '08996', 'Barcelona', '655443387');
 INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (2, 'Noa', 'Villena', 'Laietana, Via ', '54336', 'Girona', '566887790');
 INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (3, 'Uxia ', 'Martínez', 'Lírica, Carrer de la', '07665', 'Girona', '633478559');
@@ -112,6 +121,14 @@ INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_
 INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (9, 'Marcela ', 'Torregrosa', 'Nau, Carrer de la', '07763', 'Tarragona', '666645337');
 INSERT INTO `pizzeria`.`client` (`client_id`, `nom`, `cognoms`, `carrer`, `codi_postal`, `provincia`, `telefon`) VALUES (10, 'Mari ', 'Toledano ', 'Nínive, Carrer de', '01234', 'tarragona', '699885463');
 
+-- create data to comanda --
+INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (1, '2021-10-23 11:48:13', 'botiga', 1, 10, 1, 1);
+INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (2, '2021-10-23 11:48:13', 'botiga', 2, 20, 1, 2);
+INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (3, '2021-10-23 11:48:13', 'botiga', 3, 30, 3, 3);
+INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (4, '2021-10-23 11:48:13', 'domicili', 1, 10, 3, 4);
+INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (5, '2021-10-23 11:48:13', 'domicili', 2, 20, 3, 5);
+INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (6, '2021-10-23 11:48:13', 'domicili', 3, 30, 3, 6);
+INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (7, '2021-10-23 11:48:13', 'botiga', 3, 30, 4, 7);
 INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (1, '2021-10-23 11:48:13', 'botiga', 1, 10, 1, 1);
 INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (2, '2021-10-23 11:48:13', 'botiga', 2, 20, 1, 2);
 INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (3, '2021-10-23 11:48:13', 'botiga', 3, 30, 3, 3);
@@ -120,13 +137,7 @@ INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quanti
 INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (6, '2021-10-23 11:48:13', 'domicili', 3, 30, 3, 6);
 INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (7, '2021-10-23 11:48:13', 'botiga', 3, 30, 4, 7);
 
-INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (1, '2021-10-23 11:48:13', 'botiga', 1, 10, 1, 1);
-INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (2, '2021-10-23 11:48:13', 'botiga', 2, 20, 1, 2);
-INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (3, '2021-10-23 11:48:13', 'botiga', 3, 30, 3, 3);
-INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (4, '2021-10-23 11:48:13', 'domicili', 1, 10, 3, 4);
-INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (5, '2021-10-23 11:48:13', 'domicili', 2, 20, 3, 5);
-INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (6, '2021-10-23 11:48:13', 'domicili', 3, 30, 3, 6);
-INSERT INTO `pizzeria`.`comanda` (`comanda_id`, `data_hora`, `delivery`, `quantitat`, `preu`, `repartidor_id`, `client_id`) VALUES (7, '2021-10-23 11:48:13', 'botiga', 3, 30, 4, 7);
+-- create data to empleat --
 INSERT INTO `pizzeria`.`empleat` (`empleat_id`, `nom`, `cognoms`, `botiga_id`, `telefon`, `repartidor_id`) VALUES (1, 'Gerard', 'Bolaños', 1, '544334567', 1);
 INSERT INTO `pizzeria`.`empleat` (`empleat_id`, `nom`, `cognoms`, `botiga_id`, `telefon`, `repartidor_id`) VALUES (2, 'Norberto ', 'Bonet', 2, '666557788', 2);
 INSERT INTO `pizzeria`.`empleat` (`empleat_id`, `nom`, `cognoms`, `botiga_id`, `telefon`, `repartidor_id`) VALUES (3, 'Josep ', 'Moron', 3, '654321477', 3);
@@ -138,11 +149,13 @@ INSERT INTO `pizzeria`.`empleat` (`empleat_id`, `nom`, `cognoms`, `botiga_id`, `
 INSERT INTO `pizzeria`.`empleat` (`empleat_id`, `nom`, `cognoms`, `botiga_id`, `telefon`, `repartidor_id`) VALUES (9, 'Delfina ', 'Herrera', 2, '655667788', 1);
 INSERT INTO `pizzeria`.`empleat` (`empleat_id`, `nom`, `cognoms`, `botiga_id`, `telefon`, `repartidor_id`) VALUES (10, ' Lola ', 'Dueñas ', 5, '657900000', 2);
 
+-- create data to repartidor --
 INSERT INTO `pizzeria`.`repartidor` (`repartidor_id`, `nom`, `data_hora`) VALUES (1, 'Flora Gordo', '2023-01-12 11:32:13');
 INSERT INTO `pizzeria`.`repartidor` (`repartidor_id`, `nom`, `data_hora`) VALUES (2, 'Juan Plaza ', '2023-01-14 11:32:13');
 INSERT INTO `pizzeria`.`repartidor` (`repartidor_id`, `nom`, `data_hora`) VALUES (3, ' Modesto Fernandez ', '2023-01-15 11:32:13');
 INSERT INTO `pizzeria`.`repartidor` (`repartidor_id`, `nom`, `data_hora`) VALUES (4, ' Zohra da Silva ', '2023-01-13 11:32:13');
 
+-- create data to pizza --
 INSERT INTO `pizzeria`.`pizza` (`pizza_id`, `nom`) VALUES (1, 'Margarita');
 INSERT INTO `pizzeria`.`pizza` (`pizza_id`, `nom`) VALUES (2, 'Napolitana');
 INSERT INTO `pizzeria`.`pizza` (`pizza_id`, `nom`) VALUES (3, 'Marinara ');
@@ -154,6 +167,7 @@ INSERT INTO `pizzeria`.`pizza` (`pizza_id`, `nom`) VALUES (8, 'Flammkuchen');
 INSERT INTO `pizzeria`.`pizza` (`pizza_id`, `nom`) VALUES (9, 'Cuatro Quesos ');
 INSERT INTO `pizzeria`.`pizza` (`pizza_id`, `nom`) VALUES (10, 'Cuatro Estaciones ');
 
+-- create data to producte --
 INSERT INTO `pizzeria`.`producte` (`producte_id`, `nom`, `descripcio`) VALUES (1, 'Cocacola', 'Bebida');
 INSERT INTO `pizzeria`.`producte` (`producte_id`, `nom`, `descripcio`) VALUES (2, 'Limonada', 'Bebida');
 INSERT INTO `pizzeria`.`producte` (`producte_id`, `nom`, `descripcio`) VALUES (3, 'Agua', 'Bebida');
@@ -164,4 +178,16 @@ INSERT INTO `pizzeria`.`producte` (`producte_id`, `nom`, `descripcio`, `pizza_id
 INSERT INTO `pizzeria`.`producte` (`producte_id`, `nom`, `descripcio`, `pizza_id`) VALUES (8, 'Pizza', 'Comida', 2);
 INSERT INTO `pizzeria`.`producte` (`producte_id`, `nom`, `descripcio`, `pizza_id`) VALUES (9, 'Pizza', 'Comida', 3);
 INSERT INTO `pizzeria`.`producte` (`producte_id`, `nom`, `descripcio`, `pizza_id`) VALUES (10, 'Pizza', 'Comida', 4);
+
+-- Llista quants productes de tipus “Begudes”. s'han venut en una determinada localitat. --
+-- select * from producte; 
+-- select * from botiga;
+
+SELECT p.descripcio, b.localitat
+FROM producte p
+JOIN botiga b ON b.botiga_id = p.producte_id;
+
+-- Llista quantes comandes ha efectuat un determinat empleat/da. --
+
+
 
